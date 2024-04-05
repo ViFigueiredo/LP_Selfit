88ac0af9fa04cb159d431f249fae869f2c70f229
Vi Figueiredo <vifigcode@gmail.com.br>
2024-04-05 10:58:38 -0300

    remoção de vue-papa-parse

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import './assets/css/output.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
-import VuePapaParse from 'vue-papa-parse';
 
 const app = createApp(App);
 app.mount('#app');
 app.use(PrimeVue);
-app.use(VuePapaParse);
