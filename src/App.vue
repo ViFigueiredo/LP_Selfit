bd7037b578851dc00066e84b18ba428a2270aecc
Vi Figueiredo <vifigcode@gmail.com.br>
2024-04-18 11:51:31 -0300

    mobile first init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
-  <header class="w-full relative">
-    <img :src="banner" alt="banner" />
-
-    <!-- <button class="btn-zap">
-      <img src="./assets/img/whatsapp.png" alt="" />
-      <a
-        href="https://wa.me//5581981717978?text=Quero+conhecer+mais+sobre+as+academias+da+Selfit..."
-        >Fale conosco</a
-      >
-    </button> -->
+  <header class="w-full h-full">
+    <img :src="banner" class="w-full h-full" alt="banner" />
   </header>
 
-  <div class="oferta-wraper relative w-full space-x-10 bg-black">
-    <img class="" src="./assets/img/oferta.png" alt="plano plus" />
-    <button
-      class="absolute top-[45%] right-[3%] p-5 rounded-xl bg-red-600 hover:bg-gray-400 flex text-white hover:text-red-600 font-bold text-2xl"
-    >
-      <span @click="toggleModal">SELECIONAR UNIDADE</span>
-    </button>
+  <div class="oferta-wraper w-full flex flex-col bg-black">
+    <img class="w-full h-60" src="./assets/img/oferta.png" alt="plano plus" />
+    <div class="flex w-full h-[100px] justify-center items-center">
+      <button
+        class="p-3 rounded-xl bg-red-600 hover:bg-gray-400 text-white hover:text-red-600 font-bold"
+      >
+        <span @click="toggleModal">SELECIONAR UNIDADE</span>
+      </button>
+    </div>
   </div>
 
   <div
@@ -38,7 +32,11 @@
         >
           {{ cidade.nome }}
         </li>
-        <div id="unidade" class="showUnidade space-y-3" v-if="cidade.showUnidades">
+        <div
+          id="unidade"
+          class="flex flex-col text-white mt-[30px] p-[5%] border border-2 border-solid border-red-600 space-y-3"
+          v-if="cidade.showUnidades"
+        >
           <span
             v-for="unidade in unidades(cidade.nome)"
             :key="unidade"
@@ -51,8 +49,8 @@
     </div>
   </div>
 
-  <div class="w-full flex justify-center my-5 px-5 space-x-5">
-    <div id="carousel" class="relative h-80 w-[50%]">
+  <div class="w-full flex flex-col justify-center my-5 px-5 space-y-5">
+    <div id="carousel" class="relative h-80 w-full">
       <div
         v-for="(item, index) in carouselItems"
         :key="index"
@@ -64,14 +62,20 @@
           <span>{{ item.description }}</span>
         </div>
 
-        <button class="btn-carousel btn-l absolute" @click="prevSlide">
+        <button
+          class="flex justify-center items-center w-7 h-7 bg-white bg-opacity-70 rounded-full hover:bg-gray-300 top-[10px] left-[10px] absolute"
+          @click="prevSlide"
+        >
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
           />
           <span class="material-symbols-outlined"> navigate_before </span>
         </button>
-        <button class="btn-carousel btn-r absolute" @click="nextSlide">
+        <button
+          class="flex justify-center items-center w-7 h-7 bg-white bg-opacity-70 rounded-full hover:bg-gray-300 top-[10px] right-[10px] absolute"
+          @click="nextSlide"
+        >
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
@@ -81,19 +85,21 @@
       </div>
     </div>
 
-    <div id="video" class="w-[50%] rounded">
+    <div id="video" class="w-[full] rounded">
       <iframe
         src="https://www.youtube.com/embed/vh0zLxOPKB4"
         title="Selfit - Academia do seu jeito"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
-        class="w-full h-full rounded-xl"
+        class="w-full h-80 rounded-xl"
       ></iframe>
     </div>
   </div>
 
-  <footer>2024 Selfit © Todos os direitos reservados. Desenvolvido por Avantti Consultoria</footer>
+  <footer class="w-full flex justify-center items-center text-center p-5 bg-red-600 text-white">
+    2024 Selfit © Todos os direitos reservados. Desenvolvido por Avantti Consultoria
+  </footer>
 </template>
 
 <script setup lang="js">
@@ -214,81 +220,5 @@ onMounted(async () => {
   width: 100%;
   height: 100%;
   transition: opacity 0.5s ease;
-}
-
-#text-carousel {
-  width: 100%;
-}
-
-.video iframe {
-  border-radius: 15px;
-  width: 100%;
-  height: 100%;
-}
-
-.btn-carousel {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 30px;
-  height: 30px;
-  background-color: rgba(255, 255, 255, 0.719);
-  border-radius: 50%;
-}
-
-.btn-carousel:hover {
-  background-color: rgb(194, 194, 194);
-}
-
-.btn-carousel span {
-  font-size: 2.5rem;
-  font-weight: bold;
-}
-
-.btn-l {
-  top: 10px;
-  left: 10px;
-}
-
-.btn-r {
-  top: 10px;
-  right: 10px;
-}
-
-.btn-zap {
-  display: flex;
-  position: absolute;
-  top: 75%;
-  right: 10%;
-  padding: 2% 4%;
-  background-color: rgb(34, 211, 34);
-  color: white;
-  font-weight: bold;
-  font-size: 1.25rem;
-  border-radius: 15px;
-}
-
-.btn-zap img {
-  width: 30px;
-  margin-right: 10px;
-}
-
-footer {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 7px 0;
-  background-color: red;
-  color: #fff;
-}
-
-.showUnidade {
-  display: flex;
-  flex-direction: column;
-  color: white;
-  margin-top: 30px;
-  padding: 5%;
-  border: 2px solid red;
 }
 </style>
