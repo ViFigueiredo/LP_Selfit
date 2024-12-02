79cc6191aa2f2c1a0dd1821fea1c39e5b84d61d9
ViFigueiredo <vifigueiredo@outlook.com.br>
2024-12-02 09:25:24 -0300

    "Updated CSS styles and HTML structure in HomeView.vue, added new classes and styles, and modified existing ones."

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,8 @@
           <img src="./assets/img/banner_maes.png" class="w-full h-full" alt="banner" />
         </header> -->
 
-        <div id="modal-shadow" class="z-10 absolute top-0 w-full h-screen bg-red-300"
-      v-if="showModal" @click="toggleModal">
-      <div id="modal" class="bg-black overflow-y-auto p-6 w-full h-full rounded-xl flex flex-col space-y-5"
-        @click.stop>
+    <div id="modal-shadow" class="z-10 absolute top-0 w-full h-screen bg-red-300" v-if="showModal" @click="toggleModal">
+      <div id="modal" class="bg-black overflow-y-auto p-6 w-full h-full rounded-xl flex flex-col space-y-5" @click.stop>
         <div class="flex w-full justify-end space-x-5">
           <span class="text-white font-bold cursor-pointer hover:bg-white hover:text-black"
             @click="collapseAll">Recolher Todos</span>
@@ -23,14 +21,24 @@
             <div v-for="(unidades, cidade) in cidades" :key="cidade">
               <h3 class="cursor-pointer text-white hover:text-red-600 font-bold py-5"
                 @click="toggleUnidades(uf, cidade)">
-                {{ cidade }}
+                <span class="bolder text-red-600">{{ cidade }}</span>
               </h3>
               <ul v-if="showUnidades[uf] && showUnidades[uf][cidade]">
                 <li v-for="unidade in unidades" :key="unidade.cidade"
                   class="w-full flex justify-end text-white hover:bg-red-300 hover:text-black">
-                  <a :href="unidade.url" target="_blank" class="cursor-pointer font-bold p-3"
+                  <a :href="unidade.url" target="_blank" class="flex w-full cursor-pointer font-bold p-3 space-x-3"
                     @click="tracker(unidade.bairro)">
-                    {{ unidade.bairro }}
+                    <div class="flex flex-col">
+                      <span class="underline">{{ unidade.bairro }}</span>
+                      <span>{{ unidade.endereco }}</span>
+                    </div>
+                    <div class="flex items-center bg-red-600 px-3">
+                      <link rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=open_in_new" />
+                      <span class="material-symbols-outlined">
+                        double_arrow
+                      </span>
+                    </div>
                   </a>
                 </li>
               </ul>
@@ -92,11 +100,10 @@
     <div class="h-5"></div>
 
     <div class="floater-whatsapp">
-      <a
-        href="https://wa.link/uq6pis">
+      <a href="https://wa.link/uq6pis">
         <i>
-          <svg class="z-10 w-[60px] h-[60px] bg-[#25D366] rounded-full bg-green fixed bottom-5 right-5" viewBox="0 0 24 24"
-            fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg class="z-10 w-[60px] h-[60px] bg-[#25D366] rounded-full bg-green fixed bottom-5 right-5"
+            viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M17.6 6.31999C16.8669 5.58141 15.9943 4.99596 15.033 4.59767C14.0716 4.19938 13.0406 3.99622 12 3.99999C10.6089 4.00135 9.24248 4.36819 8.03771 5.06377C6.83294 5.75935 5.83208 6.75926 5.13534 7.96335C4.4386 9.16745 4.07046 10.5335 4.06776 11.9246C4.06507 13.3158 4.42793 14.6832 5.12 15.89L4 20L8.2 18.9C9.35975 19.5452 10.6629 19.8891 11.99 19.9C14.0997 19.9001 16.124 19.0668 17.6222 17.5816C19.1205 16.0965 19.9715 14.0796 19.99 11.97C19.983 10.9173 19.7682 9.87634 19.3581 8.9068C18.948 7.93725 18.3505 7.05819 17.6 6.31999ZM12 18.53C10.8177 18.5308 9.65701 18.213 8.64 17.61L8.4 17.46L5.91 18.12L6.57 15.69L6.41 15.44C5.55925 14.0667 5.24174 12.429 5.51762 10.8372C5.7935 9.24545 6.64361 7.81015 7.9069 6.80322C9.1702 5.79628 10.7589 5.28765 12.3721 5.37368C13.9853 5.4597 15.511 6.13441 16.66 7.26999C17.916 8.49818 18.635 10.1735 18.66 11.93C18.6442 13.6859 17.9355 15.3645 16.6882 16.6006C15.441 17.8366 13.756 18.5301 12 18.53ZM15.61 13.59C15.41 13.49 14.44 13.01 14.26 12.95C14.08 12.89 13.94 12.85 13.81 13.05C13.6144 13.3181 13.404 13.5751 13.18 13.82C13.07 13.96 12.95 13.97 12.75 13.82C11.6097 13.3694 10.6597 12.5394 10.06 11.47C9.85 11.12 10.26 11.14 10.64 10.39C10.6681 10.3359 10.6827 10.2759 10.6827 10.215C10.6827 10.1541 10.6681 10.0941 10.64 10.04C10.64 9.93999 10.19 8.95999 10.03 8.56999C9.87 8.17999 9.71 8.23999 9.58 8.22999H9.19C9.08895 8.23154 8.9894 8.25465 8.898 8.29776C8.8066 8.34087 8.72546 8.403 8.66 8.47999C8.43562 8.69817 8.26061 8.96191 8.14676 9.25343C8.03291 9.54495 7.98287 9.85749 8 10.17C8.0627 10.9181 8.34443 11.6311 8.81 12.22C9.6622 13.4958 10.8301 14.5293 12.2 15.22C12.9185 15.6394 13.7535 15.8148 14.58 15.72C14.8552 15.6654 15.1159 15.5535 15.345 15.3915C15.5742 15.2296 15.7667 15.0212 15.91 14.78C16.0428 14.4856 16.0846 14.1583 16.03 13.84C15.94 13.74 15.81 13.69 15.61 13.59Z"
               fill="#ffffff" />
@@ -124,9 +131,9 @@
     <footer class="relative bottom-0 w-full flex justify-center items-center text-center p-2 bg-red-600 text-white">
       <span class="w-full">
         2024 Selfit © Todos os direitos reservados. Desenvolvido por Center Soluções.
-        <span @click="toPanel()" class="text-red-600 justify-start hover:text-white hover:cursor-pointer"> >Admin< 
+        <span @click="toPanel()" class="text-red-600 justify-start hover:text-white hover:cursor-pointer"> >Admin<
+            </span>
         </span>
-      </span>
     </footer>
   </div>
 </template>
@@ -143,6 +150,7 @@ const password = ref('');
 const showLogin = ref(false);
 const showModal = ref(false);
 const selfit = ref([]);
+
 const data = [
   {
     imgSrc: 'https://www.selfitacademias.com.br/images/aselfit-intense.jpg',
@@ -180,6 +188,7 @@ const data = [
       'O Self Velo é um espaço completo para treino aeróbico e fortalecimento do sistema cardiovascular. Equipado com esteiras, bicicletas, remos, simuladores de escadas e elípticos, proporciona uma experiência abrangente para aprimorar sua resistência e saúde cardiovascular.'
   }
 ];
+
 const currentIndex = ref(0);
 const carouselItems = ref(data);
 const showCidades = ref({});
@@ -329,5 +338,15 @@ function prevSlide() {
   width: 100%;
   height: 100%;
   transition: opacity 0.5s ease;
+}
+</style>
+
+<style scoped>
+.material-symbols-outlined {
+  font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24
 }
 </style>
